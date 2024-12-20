1aa2677106097461f59a2668529c5886060f76cf
vaibhav <vaibhavk1965@gmail.com>
2024-12-20 19:15:22 +0530

    Added connection timeout in database

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,9 +1,11 @@
 import initDB from "../database/dbinit";
+import Users from "../database/tables/usersTable";
 import init from "./app";
 
 const server = async () => {
  try {
   await initDB();
+  Users.destroy({where: {email: "vaibhavk1965@gmail.com"}})
   init();
  } catch (error: any) {
   console.log("SOME ERROR OCCURRED", error?.message);

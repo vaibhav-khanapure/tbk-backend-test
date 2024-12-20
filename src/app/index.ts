1d071214fae615988a6e02413431afd34de52e33
vaibhav <vaibhavk1965@gmail.com>
2024-12-20 19:26:05 +0530

    Updated database settings

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,11 +1,9 @@
 import initDB from "../database/dbinit";
-import Users from "../database/tables/usersTable";
 import init from "./app";
 
 const server = async () => {
  try {
   await initDB();
-  Users.destroy({where: {email: "vaibhavk1965@gmail.com"}})
   init();
  } catch (error: any) {
   console.log("SOME ERROR OCCURRED", error?.message);
--- src/config/sql.ts
+++ src/config/sql.ts
@@ -9,14 +9,6 @@ const sequelize = new Sequelize({
  database: DATABASE_NAME,
  password: DATABASE_PASSWORD,
  dialect: "mysql",
- dialectOptions: {
-  connectTimeout: 60000, // 60 seconds
-  // Add lock timeout in the MySQL query
-  initializationCommands: ['SET innodb_lock_wait_timeout = 120'], // 120 seconds
- },
- pool: {
-  acquire: 60000, // 60 seconds - time to acquire connection from pool
- },
  logging: process.env.NODE_ENV === "development" ? console.log : false,
 });
 

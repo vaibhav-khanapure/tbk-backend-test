7cf69e95584e814d88f559578bf2a3ce9a8c65ae
vaibhav <vaibhavk1965@gmail.com>
2025-01-15 16:57:01 +0530

    Updated tbo api data

--- src/utils/tboTokenGeneration.ts
+++ src/utils/tboTokenGeneration.ts
@@ -6,15 +6,15 @@ import {fixflyTokenPath} from "../config/paths";
 const {TBO_AUTH_CLIENT_ID, TBO_AUTH_USERNAME, TBO_AUTH_PASSWORD, END_USER_IP, TBO_AUTH_URL} = process.env;
 
 const tboTokenGeneration = async () => {
- try {
-  const authData = {
-   ClientId: TBO_AUTH_CLIENT_ID,
-   UserName: TBO_AUTH_USERNAME,
-   Password: TBO_AUTH_PASSWORD,
-   EndUserIp: END_USER_IP,
-   LoginType: 1,
-  };
+ const authData = {
+  ClientId: TBO_AUTH_CLIENT_ID,
+  UserName: TBO_AUTH_USERNAME,
+  Password: TBO_AUTH_PASSWORD,
+  EndUserIp: END_USER_IP,
+  LoginType: 1,
+ };
 
+//  try {
   const {data} = await axios({
    method: 'post',
    headers: {
@@ -27,11 +27,12 @@ const tboTokenGeneration = async () => {
 
   process.env.TboTokenId = data?.TokenId;
   await writeFile(fixflyTokenPath, data?.TokenId);
-  return true;
- } catch (error: any) {
-  console.error("Token Generation Error", error?.message);
-  return false;
- };
+//   return {};
+//  } catch (error: any) {
+  return { url: TBO_AUTH_URL, requestData: authData, responseData: data};
+//   console.error("Token Generation Error", error?.message);
+//   return false;
+//  };
 };
 
 export default tboTokenGeneration;

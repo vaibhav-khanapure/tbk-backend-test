bec065d54a039053535507e032697da6f1010571
vaibhav <vaibhavk1965@gmail.com>
2025-01-17 21:12:51 +0530

    Updated tbk helper

--- src/helpers/allTBOApisFromLocal.ts
+++ src/helpers/allTBOApisFromLocal.ts
@@ -2,19 +2,14 @@ import { NextFunction, Request, Response } from "express";
 import { readFile } from "fs/promises";
 import { fixflyTokenPath } from "../config/paths";
 import axios from "axios";
-import { tboFlightSearchAPI } from "../utils/tboFlightAPI";
 
 const allTBOApisFromLocal = async (req: Request, res: Response, next: NextFunction) => {
- try { 
-  const body = req.body;
-
-  console.log("BODY", body);
-   
+ try {
   const token = await readFile(fixflyTokenPath, "utf-8");
   req.body.TokenId = token;
   req.body.EndUserIp = process.env.END_USER_IP;
-  const APIUrl = process.env.TBO_FLIGHT_SEARCH_API_URL;
 
+  const APIUrl = process.env.TBO_FLIGHT_SEARCH_API_URL;
   const url = req.body.url;
 
   const URL = `${APIUrl}/${url}`;

ddef954a678d19b07101b310f8d8013fd36f110b
vaibhav <vaibhavk1965@gmail.com>
2025-02-05 22:45:02 +0530

    Updated checkUser and excluded non necessary properties

--- src/controllers/authControllers/checkUser.ts
+++ src/controllers/authControllers/checkUser.ts
@@ -4,10 +4,13 @@ import Users from "../../database/tables/usersTable";
 const checkUser = async (req: Request, res: Response, next: NextFunction) => {
  try {
   const id = res.locals?.user?.id;
-  const user = await Users.findOne({where: {id}});
+  const user = await Users.findOne({
+   where: {id},
+   attributes: {exclude: ["id", "active", "disableTicket", "createdAt", "updatedAt"]}
+  });
 
-  if(!user) return res.status(404).json({message: "No user found"});
-  if(!user?.active) {
+  if (!user) return res.status(404).json({message: "No user found"});
+  if (!user?.active) {
    return res.status(400).json({message: "Please contact user admin to enable your Account"});
   };
 

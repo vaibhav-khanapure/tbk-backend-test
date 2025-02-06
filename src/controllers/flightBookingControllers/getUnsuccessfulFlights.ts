984f7df6e33eaf4cc789ffdf1d929dd569e6f943
vaibhav <vaibhavk1965@gmail.com>
2025-02-06 13:39:50 +0530

    Optimized get Unsuccessful flights controller

--- src/controllers/flightBookingControllers/getUnsuccessfulFlights.ts
+++ src/controllers/flightBookingControllers/getUnsuccessfulFlights.ts
@@ -4,7 +4,16 @@ import UnsuccessfulFlights from "../../database/tables/unsuccessfulFlightsTable"
 const getUnsuccessfulFlights = async (req: Request, res: Response, next: NextFunction) => {
  try {
   const userId = res.locals?.user?.id;
-  const data = await UnsuccessfulFlights.findAll({where: {userId}});
+  if (!userId) return res.status(400).json({message: "Unauthorized"});
+
+  const data = await UnsuccessfulFlights.findAll({
+   where: {userId},
+   raw: true,
+   attributes: {
+    exclude: ["id", "userId", "updatedAt", "paymentMethod", "RefundedUntil", "travellers", ]
+   }
+  });
+
   return res.status(200).json({data}); 
  } catch (error) {
   next(error);

32a28cc79dcc3b2750eb64d7b27ee0164375aeca
vaibhav <vaibhavk1965@gmail.com>
2025-02-21 16:15:14 +0530

    ...

--- src/controllers/authControllers/googleAuth.ts
+++ src/controllers/authControllers/googleAuth.ts
@@ -119,6 +119,7 @@ const googleAuth = async (req: Request, res: Response, next: NextFunction) => {
   // return res.status(201).json({message: "Please contact tbk to enable your account"});
   return res.status(201).json({token, user});
  } catch (error) {
+  console.log("GOOGLE AUTH========================", {error});
   next(error);
  };
 };
--- src/controllers/authControllers/verifyUser.ts
+++ src/controllers/authControllers/verifyUser.ts
@@ -84,6 +84,7 @@ const verifyUser = async (req: Request, res: Response, next: NextFunction) => {
    return res.status(200).json({user: userdata, token: userToken});
   });
  } catch (error) {
+  console.log("VERIFY ERROR===================",{firstError: error});
   next(error);
  };
 };

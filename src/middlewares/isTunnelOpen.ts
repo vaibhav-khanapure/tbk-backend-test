709f6477c4cd22a9bedb203dc9f1b8a10dd27ff6
Vaibhav <vaibhavk1965@gmail.com>
2025-03-27 07:30:55 +0530

    Added ecosystem to .gitignore

---
+++ src/middlewares/isTunnelOpen.ts
@@ -0,0 +1,8 @@
+import type {NextFunction, Request, Response} from "express";
+
+const isTunnelOpen = (req: Request, res: Response, next: NextFunction) => {
+ if (!process.env.IS_TUNNEL_ENABLED) return res.status(400).json({message: "Unauthorized"});
+ next();
+};
+
+export default isTunnelOpen;

7eb2d054de2e37b62a859ef6778aa02c17a8471d
Vaibhav <vaibhavk1965@gmail.com>
2025-03-27 07:40:45 +0530

    updated tunel

--- src/middlewares/isTunnelOpen.ts
+++ src/middlewares/isTunnelOpen.ts
@@ -1,7 +1,7 @@
 import type {NextFunction, Request, Response} from "express";
 
 const isTunnelOpen = (req: Request, res: Response, next: NextFunction) => {
- if (!process.env.IS_TUNNEL_ENABLED) return res.status(400).json({message: "Unauthorized"});
+ if (process.env.IS_TUNNEL_ENABLED !== "true") return res.status(400).json({message: "Unauthorized"});
  next();
 };
 

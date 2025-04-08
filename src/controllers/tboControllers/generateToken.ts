2e72ef04063968f5a07eec473605a1d92822aa12
Vaibhav <vaibhavk1965@gmail.com>
2025-04-08 11:54:39 +0530

    Udpated code and and if token generated it will not br shown in response

--- src/controllers/tboControllers/generateToken.ts
+++ src/controllers/tboControllers/generateToken.ts
@@ -5,7 +5,8 @@ const generateToken = async (req: Request, res: Response, next: NextFunction) =>
  try {
   const token = await tboTokenGeneration();
   if (!token) return res.status(400).json({message: "Token generation failed"});
-  return res.status(200).json({message: "Token generated Successfully", token});
+  return res.status(200).json({success: true});
+//   return res.status(200).json({message: "Token generated Successfully", token});
  } catch (error) {
   next(error);
  };

92f08ce7c5a1b205847d66362ec04fc89d26dfe8
Vaibhav <vaibhavk1965@gmail.com>
2025-06-21 14:46:09 +0530

    added cors

--- src/lib/corsOptions.ts
+++ src/lib/corsOptions.ts
@@ -6,8 +6,7 @@ const isLive = process.env.SERVER_URL === "https://tbkbackend.onrender.com";
 const origin = [process.env.CLIENT_URL as string, "https://tbkadmin3.zendsoft.com"];
 
 const corsOptions: CorsOptions = {
-//  origin: isLive ? origin : "*",
- origin: "*",
+ origin: isLive ? origin : "*",
  credentials: false
 };
 

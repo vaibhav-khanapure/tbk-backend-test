556099a0d19ffe124cf6ff70f00821893430eb84
Vaibhav <vaibhavk1965@gmail.com>
2025-08-22 18:06:32 +0530

    Added port to mysql

--- src/config/sql.ts
+++ src/config/sql.ts
@@ -1,25 +1,23 @@
-import "dotenv/config";
-import {Sequelize, type Options} from "sequelize";
-
-const {DATABASE_HOST, DATABASE_USERNAME, DATABASE_NAME, DATABASE_PASSWORD, DATABASE_LOGGING_ENABLED} = process?.env;
+import { Sequelize, type Options } from "sequelize";
 
 const config: Options = {
- host: DATABASE_HOST,
- username: DATABASE_USERNAME,
- database: DATABASE_NAME,
- password: DATABASE_PASSWORD,
- dialect: "mysql",
- logging: DATABASE_LOGGING_ENABLED ? console.log : false,
- pool: {
+  host: process.env.DATABASE_HOST,
+  username: process.env.DATABASE_USERNAME,
+  port: 3306,
+  database: process.env.DATABASE_NAME,
+  password: process.env.DATABASE_PASSWORD,
+  dialect: "mysql",
+  logging: process.env.DATABASE_LOGGING_ENABLED === "true" ? console.log : false,
+  pool: {
     max: 10, // Maximum connections
     min: 2,  // Minimum connections
     acquire: 30000, // Max time (ms) to try getting a connection before throwing error
     idle: 10000, // Time (ms) a connection stays idle before being released
- },
- retry: {
-   max: 3,
-   match: [/ECONNRESET/],
- },
+  },
+  retry: {
+    max: 3,
+    match: [/ECONNRESET/],
+  },
 };
 
 const sequelize = new Sequelize(config);

60fc5d751b0448efd48079e2c04561fc1f4630e5
Vaibhav <vaibhavk1965@gmail.com>
2025-11-06 11:22:27 +0530

    ..

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,6 +1,7 @@
 import initDB from "../database/dbinit";
 import init from "./app";
 import https from 'node:https';
+import os from "node:os";
 
 const server = async () => {
  try {
@@ -21,3 +22,23 @@ https.get('https://api.ipify.org?format=json', res => {
     console.log('Public IP:', JSON.parse(data).ip);
   });
 });
+
+function getServerIp() {
+  const nets = os.networkInterfaces();
+  let serverIp = "127.0.0.1";
+
+  for (const name of Object.keys(nets)) {
+    for (const net of nets[name]) {
+      // Check for IPv4 and skip internal (i.e. 127.0.0.1)
+      if (net.family === "IPv4" && !net.internal) {
+        serverIp = net.address;
+        break;
+      }
+    }
+    if (serverIp !== "127.0.0.1") break;
+  }
+
+  return serverIp;
+}
+
+console.log("Server IP is ----------- :", getServerIp());

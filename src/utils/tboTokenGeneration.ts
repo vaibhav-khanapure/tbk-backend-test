cba714f52a3c5592945ac650f2692bda03b6cf71
Vaibhav <vaibhavk1965@gmail.com>
2025-11-11 17:36:26 +0530

    Removed console log

--- src/utils/tboTokenGeneration.ts
+++ src/utils/tboTokenGeneration.ts
@@ -4,15 +4,13 @@ import {writeFile} from "fs/promises";
 import {fixflyTokenPath} from "../config/paths";
 import Settings from "../database/tables/settingsTable";
 
-const {TBO_AUTH_CLIENT_ID, TBO_AUTH_USERNAME, TBO_AUTH_PASSWORD, END_USER_IP, TBO_AUTH_URL} = process.env;
-
 const tboTokenGeneration = async () => {
  try {
   const authData = {
-   ClientId: TBO_AUTH_CLIENT_ID,
-   UserName: TBO_AUTH_USERNAME,
-   Password: TBO_AUTH_PASSWORD,
-   EndUserIp: END_USER_IP,
+   ClientId: process.env?.TBO_AUTH_CLIENT_ID,
+   UserName: process.env?.TBO_AUTH_USERNAME,
+   Password: process.env?.TBO_AUTH_PASSWORD,
+   EndUserIp: process.env?.END_USER_IP,
    LoginType: 1,
   };
 
@@ -22,7 +20,7 @@ const tboTokenGeneration = async () => {
     Accept: 'application/json',
     'Content-Type': 'application/json;charset=UTF-8',
    },
-   url: TBO_AUTH_URL,
+   url: process.env?.TBO_AUTH_URL,
    data: authData,
   });
 
@@ -32,8 +30,6 @@ const tboTokenGeneration = async () => {
    writeFile(fixflyTokenPath, data?.TokenId),
    Settings.update({value: data?.TokenId}, {where: {key: "fixflyToken"}}),
   ]);
-
-  console.log("DATA IS TOKEN", data);
 
   return data?.TokenId;
  } catch (error: any) {
